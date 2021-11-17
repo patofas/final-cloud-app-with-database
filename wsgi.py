c7d1ba86f693313c02ea40efc9f6ef989dece38e
Patricio <pfagalde@gmail.com>
2021-11-17 04:31:17 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlinecourse.settings")
 
 application = Cling(get_wsgi_application())
